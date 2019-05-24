b39548a1503436d4a41f3d440d1e8154eaf52412
kyjyeon <kyjyeon93@gmail.com>
2019-05-24 18:50:18 +0900

    Creating mongodb database and inserted jokes.json datas into mongodb database('jokeapi')

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const dboperation = require('./operations');
+const jokedoc = require('./jokes/jokes.json');
+const url = 'mongodb://localhost:27017/';
+const dbname =  'jokeapi';
+const collec = 'jokes';
+
+var url = "mongodb://localhost:27017/jokeapi";
+
+// Code for Creating database
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   console.log("Database created!");
+//   db.close();
+// });
+
+// Code for Creating a Collection
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("jokeapi");
+//     dbo.createCollection("joke", function(err, res) {
+//       if (err) throw err;
+//       console.log("Collection created!");
+//       db.close();
+//     });
+//   });
+
+MongoClient.connect(url).then((client) => {
+
+    console.log('Connected correctly to server');
+    const db = client.db(dbname);
+
+    dboperation.insertDocument(db, jokedoc, collec)
+        .then((result) => {
+            console.log("Inserted Document:\n", result.ops);
+
+            return dboperation.findDocuments(db, collec);
+        })
+        .catch((err) => console.log(err));
+
+})
+.catch((err) => console.log(err));
---
+++ operations.js
@@ -0,0 +1,22 @@
+//will encapsulate all that database operations
+const assert = require('assert');
+
+exports.insertDocument = (db, document, collection, callback) =>{
+    const coll = db.collection(collection);
+    return coll.insert(document);
+};
+
+exports.findDocuments = (db, collection, callback)=>{
+    const coll = db.collection(collection);
+    return coll.find({}).toArray();
+}
+
+exports.removeDocuments = (db, document , collection, callback)=>{
+    const coll = db.collection(collection);
+    return coll.deleteone(document);
+}
+
+exports.updateDocuments = (db, document , update ,collection, callback)=>{
+    const coll = db.collection(collection);
+    return coll.updateOne(document, {$set: update}, null);
+}
