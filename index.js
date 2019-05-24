e74d0761cf21b55d4932524a6879870158f1dc76
kyjyeon <kyjyeon93@gmail.com>
2019-05-24 14:22:34 +0900

    Seperating functions file to Router folder and exporting to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
+const route = require('./Routers/route');
 
 const bot = new SlackBot({
     token : "xoxb-582582124755-587875604934-rRhFVlXlB0StEMnlrmsQlcac",
@@ -34,67 +35,32 @@ bot.on('message', (data) => {
 // Responding to Data
 function handleMessage(message){
     if(message.includes('chucknorris')){
-        chuckJoke();
+        route.chuckJoke()
+        .then((err, res)=>{
+            if(err)throw err;
+            res("ChuckJoke function activated");
+        });
     }
     else if(message.includes(' yomama')){
-        yoMamaJoke();
+        route.yoMamaJoke()
+        .then((err, res)=>{
+            if(err)throw err;
+            res("yoMamaJoke function activated");
+});
     }
     else if(message.includes(' random')){
-        randomJoke();
-    }
+        route.randomJoke()
+        .then((err, res)=>{
+            if(err)throw err
+            res("randomJoke function activated");    
+    });
+}
+
     else if(message.includes(' help')){
-        runHelp();
-    }
-}
-
-
-// Tell a Chuck Norris Joke
-function chuckJoke(){
-    axios.get('http://api.icndb.com/jokes/random/')
-    .then(res =>{
-        const joke = res.data.value.joke;
-        
-        const face = {
-            icon_emoji: ':laughing:'
-        };
-    
-        bot.postMessageToChannel('everyone', `Chuck Norris: ${joke}`,face);
-        bot.postMessageToChannel('full-stack-web', `Yo mama: ${joke}`,face);
-        bot.postMessageToChannel('bot_test', `Yo mama: ${joke}`,face);
+        route.runHelp()
+        .then((err, res)=>{
+            if(err)throw err
+            res(" function activated");  
     });
-}
-
-// Tell a yomama Joke
-function yoMamaJoke(){
-    axios.get('http://api.yomomma.info/')
-    .then(res =>{
-        const joke = res.data.joke;
-        
-        const face = {
-            icon_emoji: ':laughing:'
-        };
-        
-        bot.postMessageToChannel('everyone', `Yo mama: ${joke}`,face);
-        bot.postMessageToChannel('full-stack-web', `Yo mama: ${joke}`,face);
-        bot.postMessageToChannel('bot_test', `Yo mama: ${joke}`,face);
-       
-    });
-}
-//Tell random joke
-function randomJoke(){
-    const rand = Math.floor(Math.random() * 2) +1;
-    if(rand ===1){
-        chuckJoke();
     }
-    else if(rand === 2){
-        yoMamaJoke();
-    }
-}
-function runHelp(){
-    const face = {
-        icon_emoji: ':question:'
-    };
-
-    bot.postMessageToChannel('everyone', "Type @joker and write a joke that you would like\n ex- @joker random",face);
-    bot.postMessageToChannel('full-stack-web', "Type @joker and write a joke that you would like\n ex- @joker random",face);
 }
