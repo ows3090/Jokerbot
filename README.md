a290007920a527da6ba39ff702dafd34de57458b
kyjyeon <kyjyeon93@gmail.com>
2019-05-28 15:18:33 +0900

    Changing README screenshot
    Former-commit-id: 096ad4b69a3221044d39bbc89734bfc97d3668a1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 <h1>Building a Slackbot</h1><br>
 <h2>A slackbot that reacts to user's chat </h2><br>
 
-![joker2](https://user-images.githubusercontent.com/39256000/58410894-15ce0980-80ae-11e9-94c5-aba5f2478f84.jpg)
+![joker_fix](https://user-images.githubusercontent.com/39256000/58455581-0ce84100-815d-11e9-833c-90c7af08f52f.jpg)
--- Routers/route.js
+++ Routers/route.js
@@ -300,51 +300,5 @@ runHelp = () =>{
     comment = "Thanks for using Joker bot!:ghost::ghost:laugh:\nBot info: type '@joker --help'\nBot functions: @joker tell me [something] "
     bot.postMessageToChannel('everyone', "Type @joker and write a joke that you would like\n ex- @joker random",face);
     
+    }
 }
-}
-
-
-        // function generalJoke(){
-        //     dboperation.getdata(dbname, collec, "general")
-        //     .then((data) =>{
-        //         var query = { state: 'OK' };
-        //         var n = data.count(query);
-        //         var r = Math.floor(Math.random() * n);
-        //         var randomElement = data.find(query).limit(1).skip(r);
-        //         var question = randomElement.setup;
-        //         var joke = randomElement.punchline;
-        //         const face = {
-        //             icon_emoji: ':laughing:'
-        //         };
-            
-        //         bot.postMessageToChannel('everyone', question, joke, face);
-        //         bot.postMessageToChannel('full-stack-web', question, joke, face);
-        //         bot.postMessageToChannel('bot_test', question, joke, face);
-        //     });
-        // };
-        
-        // // Tell a yomama Joke
-        // function yomamaJoke(){
-        //     axios.get('http://api.yomomma.info/')
-        //     .then(res =>{
-        //         const joke = res.data.joke;
-                
-        //         const face = {
-        //             icon_emoji: ':laughing:'
-        //         };
-                
-        //         bot.postMessageToChannel('everyone', `Yo mama: ${joke}`,face);
-        //         bot.postMessageToChannel('full-stack-web', `Yo mama: ${joke}`,face);
-        //         bot.postMessageToChannel('bot_test', `Yo mama: ${joke}`,face);
-               
-        //     });
-        // };
-        // //Tell random joke
-        // function runhelp(){
-        //     const face = {
-        //         icon_emoji: ':question:'
-        //     };
-        
-        //     bot.postMessageToChannel('everyone', "Type @joker and write a joke that you would like\n ex- @joker random",face);
-        //     bot.postMessageToChannel('full-stack-web', "Type @joker and write a joke that you would like\n ex- @joker random",face);
-        // }
